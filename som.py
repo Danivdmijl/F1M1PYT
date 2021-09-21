37026b89f6faa9dff10f34a0494da03e387942ab
Danivdmijl <32570@ma-web.nl>
2021-09-21 12:20:10 +0200

    commit

---
+++ som.py
@@ -0,0 +1,17 @@
+som = 5678-4321
+print(som)
+som2 = 433434*13421
+print(som2)
+
+stringa = "Hallo Developers!"
+print(stringa[6:9])
+
+a = "Firetruck"
+print(a[0:1] + a[6:9])
+
+b = "Heeeianooo"
+print(b[4:7])
+
+print("Voer je naam in:")
+x = input()
+print("Goedemorgen, " + x)
