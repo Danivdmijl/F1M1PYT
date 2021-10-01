7d2a8f9a07ea0c6ab4efbcb37da9992a3a31a996
Danivdmijl <32570@ma-web.nl>
2021-10-01 10:51:13 +0200

    shuffle

---
+++ shuffle.py
@@ -0,0 +1,13 @@
+import random
+
+original = input()
+randomised = ''.join(random.sample(original, len(original)))
+print(randomised)
+
+original = input()
+randomised = ''.join(random.sample(original, len(original)))
+print(randomised)
+
+original = input()
+randomised = ''.join(random.sample(original, len(original)))
+print(randomised)
